8fb342bd8f1f43d9de7016853b8b143887b55f2a
Frits Sweijen <tikk3r@users.noreply.github.com>
2024-12-26 18:28:10 +0000

    again

---
+++ pkg/wasm_demo_bg.wasm.d.ts
@@ -0,0 +1,18 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_chart_free: (a: number, b: number) => void;
+export const __wbg_point_free: (a: number, b: number) => void;
+export const __wbg_get_point_x: (a: number) => number;
+export const __wbg_set_point_x: (a: number, b: number) => void;
+export const __wbg_get_point_y: (a: number) => number;
+export const __wbg_set_point_y: (a: number, b: number) => void;
+export const chart_mandelbrot: (a: number, b: number) => void;
+export const chart_plot3d: (a: number, b: number, c: number, d: number) => void;
+export const chart_plot_interferometer_monochrome: (a: number, b: number, c: number, d: number, e: number, f: number) => void;
+export const chart_plot_interferometer_uvcoverage: (a: number, b: number, c: number, d: number, e: number, f: number, g: number, h: number) => void;
+export const chart_coord: (a: number, b: number, c: number) => number;
+export const __wbindgen_exn_store: (a: number) => void;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_add_to_stack_pointer: (a: number) => number;
